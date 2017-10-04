fe4566dc6b1be101102acf6f8ccc1a3ca13d4ad4
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2017-10-04 17:17:16 +0530

    Adds cloudflare cache busting after deployment

---
+++ purge_cf_cache.sh
@@ -0,0 +1,5 @@
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" \
+     -H "X-Auth-Email: $CF_EMAIL" \
+     -H "X-Auth-Key: $CF_API_KEY" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
