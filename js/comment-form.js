8cb63934d346ee5c94c43096fb0e17a6af68a1f6
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2017-10-10 21:05:12 +0530

    Adds recaptcha to comment form

--- js/comment-form.js
+++ js/comment-form.js
@@ -2,9 +2,24 @@
   function addCommentSubmitHandler() {
     if (typeof jQuery !== 'undefined') {
       var jq = jQuery;
+
+      var formMessage = jq("#comment-form-message");
+      var toutId = null;
+      function showMsg(msg) {
+        formMessage.html(msg).fadeIn("slow");
+        if (toutId !== null) {
+          window.clearTimeout(toutId);
+        }
+        toutId = window.setTimeout(function() { formMessage.fadeOut("slow"); }, 5000);
+      }
+
       jq("#comment-redirect").remove();
       jq("#comment-form").on("submit", function(event) {
         event.preventDefault();
+        if (grecaptcha.getResponse() === "") {
+          showMsg("Please tick the reCAPTCHA");
+          return false;
+        }
         var form = jq(this);
         var reqBody = form.serialize();
         jq("#comment-form fieldset").attr("disabled", "disabled");
@@ -20,17 +35,16 @@
             "Content-Type": "application/x-www-form-urlencoded"
           }})
         .then(function(response) {
-          var formMessage = jq("#comment-form-message");
           formSubmit.val(origVal);
           jq("#comment-form fieldset").removeAttr("disabled");
 
           if (!response.ok) {
-            formMessage.html("Sorry, there was an error in posting the comment.").fadeIn("slow");
+            showMsg("Sorry, there was an error in posting the comment.");
           } else {
             form.trigger("reset");
-            formMessage.html("Thanks for your comment. It will show on the site once it has been approved.").fadeIn("slow");
+            grecaptcha.reset();
+            showMsg("Thanks for your comment. It will show on the site once it has been approved.");
           }
-          window.setTimeout(function() { formMessage.fadeOut("slow"); }, 5000);
         });
         return false;
       });
