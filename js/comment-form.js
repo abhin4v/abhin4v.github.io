0d57b139d257bef59bc553deefdd60cdea8fbed2
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2017-10-11 10:30:25 +0530

    Adds timeout for submitting comment

--- js/comment-form.js
+++ js/comment-form.js
@@ -3,6 +3,15 @@
     if (typeof jQuery !== 'undefined') {
       var jq = jQuery;
 
+      function timeout(ms, promise) {
+        return new Promise(function(resolve, reject) {
+          window.setTimeout(function() {
+            reject(new Error("timeout"))
+          }, ms)
+          promise.then(resolve, reject)
+        })
+      }
+
       var formMessage = jq("#comment-form-message");
       var toutId = null;
       function showMsg(msg) {
@@ -21,30 +30,37 @@
           return false;
         }
         var form = jq(this);
+        var fieldSet = jq("#comment-form fieldset");
         var reqBody = form.serialize();
-        jq("#comment-form fieldset").attr("disabled", "disabled");
+        fieldSet.attr("disabled", "disabled");
 
         var formSubmit = jq("#comment-form-submit");
         var origVal = formSubmit.val();
         formSubmit.val("Posting ...");
 
-        fetch(form.attr("action"), {
+        timeout(15000, fetch(form.attr("action"), {
           method: form.attr("method"),
           body: reqBody,
           headers: {
             "Content-Type": "application/x-www-form-urlencoded"
-          }})
+          }}))
         .then(function(response) {
           formSubmit.val(origVal);
-          jq("#comment-form fieldset").removeAttr("disabled");
+          fieldSet.removeAttr("disabled");
 
           if (!response.ok) {
-            showMsg("Sorry, there was an error in posting the comment.");
+            showMsg("Sorry, there was an error in posting the comment. Please try again.");
           } else {
             form.trigger("reset");
             grecaptcha.reset();
             showMsg("Thanks for your comment. It will show on the site once it has been approved.");
           }
+        })
+        .catch(function(error) {
+          formSubmit.val(origVal);
+          fieldSet.removeAttr("disabled");
+          grecaptcha.reset();
+          showMsg("Sorry, there was an error in posting the comment. Please try again.");
         });
         return false;
       });
