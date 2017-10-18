c357eb2b80e013805bb12983d351f42d319c0222
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2017-10-18 09:01:28 +0530

    Fixes PDF generation to always generate missing PDFs.

--- bin/generate_pdfs.sh
+++ bin/generate_pdfs.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 set -x #echo on
-git status --short  | grep "posts/.*.html" | awk '{print $2}' | sed 's/\/index.html//' > changed-posts
-$HOME/.local/bin/site server &
 mkdir -p pdfs/posts
-cat changed-posts | xargs -r -t -n1 -I {} chrome-headless-render-pdf --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
+$HOME/.local/bin/site server &
+ls -1 -d posts/*/ | sed 's/\/$//' | xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
 killall site
 git add --all pdfs
