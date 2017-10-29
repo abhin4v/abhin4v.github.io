6cf03d96cdf54795c78485bffd28fb55b9eedbd9
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2017-10-29 17:24:54 +0530

    Adds sleep in PDF generation and fixes print style.

--- bin/generate_pdfs.sh
+++ bin/generate_pdfs.sh
@@ -2,7 +2,9 @@
 
 set -x #echo on
 mkdir -p pdfs/posts
-$HOME/.local/bin/site server &
+python -m SimpleHTTPServer &
+SERVER_PID=$!
+sleep 5
 ls -1 -d posts/*/ | sed 's/\/$//' | xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
-killall site
+kill $SERVER_PID
 git add --all pdfs
