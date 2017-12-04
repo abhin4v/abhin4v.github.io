e27d5f3088a5e4d1e763f13b24b3d98a673ba891
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2017-12-04 17:08:54 +0530

    Removes duration from activities.json

--- js/activities.js
+++ js/activities.js
@@ -1,6 +1,6 @@
 (function() {
   function showActivitiesCalendar() {
-    if (typeof jQuery !== 'undefined') {
+    if (typeof jQuery !== "undefined") {
       function renderCalendar(activities, type, colorRange, containerId) {
         var chartData = [];
         var i = -1;
@@ -10,8 +10,8 @@
             return;
           }
 
-          var date = moment(a.date).startOf('day').toDate();
-          var count= a.distance;
+          var date = moment(a.date).startOf("day").toDate();
+          var count = a.distance;
 
           if ((lastDate != null) && (lastDate.getTime() === date.getTime())) {
             chartData[i].count += count;
@@ -21,20 +21,21 @@
             lastDate = date;
           }
         });
+
         chartData.forEach(function(d) {
           d.count = Math.round(d.count/1000);
-        })
+        });
 
         var activitiesHeatmap = calendarHeatmap()
-                               .data(chartData)
-                               .selector(containerId)
-                               .colorRange(colorRange)
-                               .tooltipEnabled(true)
-                               .tooltipUnit(
-                                  [{min: 0, unit: type.toLowerCase() + 's'},
-                                   {min: 1, max: 1, unit: 'km'},
-                                   {min: 2, max: 'Infinity', unit: 'kms'}
-                                  ]);
+                                .data(chartData)
+                                .selector(containerId)
+                                .colorRange(colorRange)
+                                .tooltipEnabled(true)
+                                .tooltipUnit([
+                                  {min: 0, unit: type.toLowerCase() + "s"},
+                                  {min: 1, max: 1, unit: "km"},
+                                  {min: 2, max: "Infinity", unit: "kms"}
+                                ]);
         activitiesHeatmap();
         jQuery(containerId).css("display", "block");
       }
@@ -47,7 +48,7 @@
           return response.json();
         })
         .then(function(activities) {
-          renderCalendar(activities, "Ride", ['#FFF7F4', '#FF4B08'], "#ride-calendar");
+          renderCalendar(activities, "Ride", ["#FFF7F4", "#FF4B08"], "#ride-calendar");
           renderCalendar(activities, "Run", ["#F9F5FF", "#6B42C0"], "#run-calendar");
 
           jQuery("svg.calendar-heatmap")
