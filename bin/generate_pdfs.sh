a3092fa05340c0fd27c11460867e889d021b551d
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2018-06-20 11:51:33 +0530

    Upgrade node version and fixes pdf generation

--- bin/generate_pdfs.sh
+++ bin/generate_pdfs.sh
@@ -5,6 +5,6 @@ mkdir -p pdfs/posts
 python -m SimpleHTTPServer &
 SERVER_PID=$!
 sleep 5
-ls -1 -d posts/*/ | sed 's/\/$//' | xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
+git status --short | grep "posts/.*.html" | cut -c 4- | sed 's/\/index.html//' | xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --chrome-option=--no-sandbox --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
 kill $SERVER_PID
 git add --all pdfs
