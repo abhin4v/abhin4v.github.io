de62e0fe996007466b17ff36cf6522ad4e098f40
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2018-06-28 09:13:30 +0530

    Ignore drafts when generating PDFs

--- bin/generate_pdfs.sh
+++ bin/generate_pdfs.sh
@@ -5,6 +5,13 @@ mkdir -p pdfs/posts
 python3 -m http.server &
 SERVER_PID=$!
 sleep 5
-git status --short | grep "posts/.*.html" | cut -c 4- | sed 's/\/index.html//' | xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --chrome-option=--no-sandbox --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
+git status --short | \
+  grep "posts/.*" | \
+  grep -v drafts | \
+  cut -c 4- | \
+  sed 's/\/index.html//' | \
+  sed 's/.md//' | \
+  sort -u | \
+  xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --chrome-option=--no-sandbox --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
 kill $SERVER_PID
 git add --all pdfs
