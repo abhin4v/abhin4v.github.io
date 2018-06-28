0bd195bbc5d1cc80f12371aa873d1d38a06c5cc7
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2018-06-28 11:53:01 +0530

    Changed PDF generation script to take care of styling changes.

--- bin/generate_pdfs.sh
+++ bin/generate_pdfs.sh
@@ -5,13 +5,23 @@ mkdir -p pdfs/posts
 python3 -m http.server &
 SERVER_PID=$!
 sleep 5
-git status --short | \
-  grep "posts/.*" | \
-  grep -v drafts | \
-  cut -c 4- | \
-  sed 's/\/index.html//' | \
-  sed 's/.md//' | \
-  sort -u | \
-  xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --chrome-option=--no-sandbox --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
+
+CSS_CHANGED=`git status --short | grep "css/"`
+if [ -z "$CSS_CHANGED" -a "$CSS_CHANGED" == "" ]; then
+  git status --short | \
+    grep "posts/.*" | \
+    grep -v drafts | \
+    grep -v "pdfs/posts" | \
+    cut -c 4- | \
+    sed 's/\/index.html//' | \
+    sed 's/.md//' | \
+    sort -u \ |
+    xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --chrome-option=--no-sandbox --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
+else
+  ls -1 -d posts/*/ | \
+    sed 's/\/$//' | \
+    xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --chrome-option=--no-sandbox --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
+fi
+
 kill $SERVER_PID
 git add --all pdfs
