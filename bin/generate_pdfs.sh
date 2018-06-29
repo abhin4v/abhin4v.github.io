6c9b735a8ff10972e779d71a4ed8502b078c93ab
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2018-06-30 01:06:14 +0530

    Fixes PDF gen script

--- bin/generate_pdfs.sh
+++ bin/generate_pdfs.sh
@@ -17,7 +17,7 @@ if [ -z "$CSS_CHANGED" -a "$CSS_CHANGED" == "" ]; then
     cut -c 4- | \
     sed 's/\/index.html//' | \
     sed 's/.md//' | \
-    sort -u \ |
+    sort -u | \
     xargs -r -t -n1 -I {} ../node_modules/.bin/chrome-headless-render-pdf --chrome-option=--no-sandbox --url http://127.0.0.1:8000/{}/ --pdf pdfs/{}.pdf
 else
   ls -1 -d posts/*/ | \
