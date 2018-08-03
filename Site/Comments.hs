bd9e6fd1d695a485a85d1c5ad392a81b5be205fa
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2018-08-03 13:27:48 +0530

    Adds support for displaying nested comments

--- Site/Comments.hs
+++ Site/Comments.hs
@@ -1,13 +1,16 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
 module Site.Comments where
 
+import qualified Data.Map as Map
 import Data.Time.Clock (UTCTime)
 import Data.Time.Format (formatTime, defaultTimeLocale, parseTimeM, iso8601DateFormat)
 import Hakyll
 import Site.PostCompiler
 
 comments :: Rules ()
-comments =
+comments = do
+  replyToMap <- commentReplyToMap =<< getMatches "comments/*/*.md"
+
   match "comments/*/*.md" $
     compile $ do
       ident           <- getUnderlying
@@ -16,8 +19,16 @@ comments =
       email           <- getMetadataField' ident "email"
       commentID       <- getMetadataField' ident "_id"
       let dateS       = formatTime defaultTimeLocale "%B %e, %Y" date
+          level       = calcLevel replyToMap ident
 
       getResourceBody
         >>= renderPandocWith readerOptions writerOptions
-        >>= loadAndApplyTemplate "templates/comment.html" (commentCtx commentID dateS tss email)
+        >>= loadAndApplyTemplate "templates/comment.html" (commentCtx commentID dateS tss email level)
         >>= saveSnapshot "comment"
+
+calcLevel :: Map.Map Identifier Identifier -> Identifier -> Int
+calcLevel replyToMap comment = go comment 0
+  where
+    go c l = case Map.lookup c replyToMap of
+      Nothing -> l
+      Just pc -> go pc (l+1)
--- Site/PostCompiler.hs
+++ Site/PostCompiler.hs
@@ -8,12 +8,16 @@ module Site.PostCompiler
   , postCtx
   , postCtxWithTags
   , commentCtx
+  , commentReplyToMap
   ) where
 
-import Data.List (sortBy, intercalate)
-import Data.Maybe (fromMaybe)
+import Control.Monad (foldM)
+import Data.List (stripPrefix, sortBy, intercalate, (\\))
+import Data.Maybe (fromJust, fromMaybe)
+import qualified Data.Map.Strict as Map
 import Data.Monoid ((<>))
 import Data.Ord (comparing)
+import qualified Data.Tree as Tree
 import Hakyll hiding (relativizeUrls)
 import Site.ERT
 import Site.TOC
@@ -86,21 +90,50 @@ postCtx =
 
     tagCtx = field "tag" (return . itemBody)
 
-commentCtx :: String -> String -> String -> String -> Context String
-commentCtx commentID date datetime email =
+commentCtx :: String -> String -> String -> String -> Int -> Context String
+commentCtx commentID date datetime email level =
   constField "id" commentID <>
   constField "date" date <>
   constField "datetime" datetime <>
   constField "email" email <>
+  constField "level" (show level) <>
   siteContext
 
 postCtxWithTags :: Tags -> Context String
 postCtxWithTags tags = tagsField "tags" tags <> postCtx
 
+commentReplyToMap :: MonadMetadata m => [Identifier] -> m (Map.Map Identifier Identifier)
+commentReplyToMap comments =
+  flip (`foldM` Map.empty) comments $ \acc comment -> do
+    let postSlug = fromJust . stripPrefix "comments/" . takeDirectory . toFilePath $ comment
+    replyToID <- getMetadataField' comment "reply_to"
+    case replyToID of
+      "" -> return acc
+      _  -> do
+        parentComments <- getMatches $ fromGlob $ "comments/" <> postSlug <> "/" <> replyToID <> ".md"
+        case parentComments of
+          []              -> return acc
+          [parentComment] -> return $ Map.insert comment parentComment acc
+          _               -> error "Impossibru"
+
 sortComments :: MonadMetadata m => [Item a] -> m [Item a]
-sortComments = sortByM $ flip getMetadataField' "date" . itemIdentifier
+sortComments items = do
+  let itemIdentifierMap =
+        foldl (\acc i -> Map.insert (itemIdentifier i) i acc) Map.empty items
+
+  replyToMap <- commentReplyToMap . map itemIdentifier $ items
+  let replyFromMap =
+        Map.foldlWithKey (\acc k v -> Map.insertWith (++) v [k] acc) Map.empty replyToMap
+
+  topLevelCommentIDs <- sortByDate $ Map.keys itemIdentifierMap \\ Map.keys replyToMap
+  commentIDForest    <- flip Tree.unfoldForestM_BF topLevelCommentIDs $ \commentID ->
+    (commentID,) <$> sortByDate (Map.findWithDefault [] commentID replyFromMap)
+
+  return . map (itemIdentifierMap Map.!) . concatMap Tree.flatten $ commentIDForest
   where
-    sortByM f xs = map fst . sortBy (comparing snd) <$> mapM (\ x -> (x,) <$> f x) xs
+    sortByDate xs =
+      map fst . sortBy (comparing snd)
+      <$> mapM (\ x -> (x,) <$> getMetadataField' x "date") xs
 
 itemAfter, itemBefore :: Eq a => [a] -> a -> Maybe a
 itemAfter xs x  = lookup x $ zip xs (tail xs)
