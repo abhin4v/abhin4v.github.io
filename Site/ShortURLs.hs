6c28c151cee00302a6124ccf256d2a91a8136954
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2019-01-27 12:15:06 +0530

    Moves shortURL root out of mappings

--- Site/ShortURLs.hs
+++ Site/ShortURLs.hs
@@ -6,12 +6,12 @@ import Site.Util
 
 urlMappings :: [(String, String)]
 urlMappings = [
-    ("s/ccy", "/talks/clojure-concurrency-you/")
+    ("ccy", "/talks/clojure-concurrency-you/")
   ]
 
 shortURLs :: Rules ()
 shortURLs =
   forM_ urlMappings $ \(su, lu) ->
-    create [fromFilePath $ su ++ ".html"] $ do
+    create [fromFilePath $ "s/" ++ su ++ ".html"] $ do
       route indexHTMLRoute
       compile $ makeItem $ Redirect lu
