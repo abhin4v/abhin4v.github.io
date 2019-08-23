eca433a358f6f6e972a8ed9344fb2ac2f76de612
Abhinav Sarkar <abhinav@abhinavsarkar.net>
2019-08-23 12:35:01 +0530

    Adds now page history

--- Site/Pages.hs
+++ Site/Pages.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Site.Pages where
 
+import Data.List (sortOn)
+import Data.Maybe (catMaybes)
+import qualified Data.Ord
 import Hakyll hiding (relativizeUrls)
 import Site.Pandoc
 import Site.PostCompiler
@@ -11,17 +14,38 @@ import Text.Pandoc.Walk (walk)
 pages :: String -> Rules ()
 pages env = do
   -- about page
-  page "about" "about.md" env
-
-  -- now page
-  page "now" "now.md" env
+  page "about" (fromList ["about.md"]) env
 
   -- projects page
-  page "projects" "projects.md" env
+  page "projects" (fromList ["projects.md"]) env
 
-page :: String -> Identifier -> String -> Rules ()
-page pageName pageFile env =
-  match (fromList [pageFile]) $ do
+  nowPages env
+
+nowPages :: String -> Rules ()
+nowPages env = do
+  page "now" "now/*.md" env
+
+  match (fromList ["now.md"]) $ do
+    nows <- sortOn Data.Ord.Down <$> getMatches "now/*.md"
+
+    route indexHTMLRoute
+    compile $ do
+      nowPaths <- map removeIndexURL . catMaybes <$> mapM getRoute nows
+      let ctx = listField "nows" nowCtx (mapM makeItem nowPaths) <> siteContext
+
+      contentCompiler (pageContentTransforms "now")
+        >>= loadAndApplyTemplate "templates/now.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" siteContext
+        >>= relativizeUrls env
+  where
+    nowCtx = field "path" (return . itemBody) <>
+             field "date" (return . getDate . itemBody)
+
+    getDate = take 10 . drop 4
+
+page :: String -> Pattern -> String -> Rules ()
+page pageName pat env =
+  match pat $ do
     route indexHTMLRoute
     compile $ contentCompiler (pageContentTransforms pageName)
       >>= loadAndApplyTemplate "templates/default.html" siteContext
